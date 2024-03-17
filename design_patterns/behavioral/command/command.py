214782b76789a0afbb90013c2f372af1ce932e98
Gary Yao <garyjyao@gmail.com>
2024-03-17 12:05:51 +1100

    feat: add command pattern with tests

---
+++ design_patterns/behavioral/command/command.py
@@ -0,0 +1,35 @@
+class SearchRequest:
+    pass
+
+
+class SearchResponse:
+    def __init__(self, request):
+        self._request = request
+
+    def get_request(self):
+        return self._request
+
+
+class HttpClient:
+    def post(self, request):
+        return SearchResponse(request)
+
+
+class SearchCommand:
+    def __init__(self, search_request, http_client):
+        self._http_client = http_client
+        self._search_request = search_request
+
+    def execute(self):
+        return self._http_client.post(self._search_request)
+
+
+# Demonstrate if you want to complete Object-Oriented like Java
+# class CommandPattern:
+#     @staticmethod
+#     def main():
+#         SearchCommand(SearchRequest(), HttpClient()).execute()
+
+
+if __name__ == "__main__":
+    SearchCommand(SearchRequest(), HttpClient()).execute()
