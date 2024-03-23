05b2add8ede298047c14b21bf472741532412b80
Gary Yao <garyjyao@gmail.com>
2024-03-23 17:43:56 +1100

    feat: facade pattern

---
+++ design_patterns/structural/facade_pattern/facade_pattern.py
@@ -0,0 +1,41 @@
+# In summary, the Car class acts as a facade, hiding the complexity of the subsystems
+# and providing a simple interface to the client, which in this case is the FacadePattern class.
+# This makes the client code easier to read and maintain.
+
+class IntakeSystem:
+    def open_throttle(self):
+        pass
+
+
+class FuelSystem:
+    def inject_fuel(self):
+        pass
+
+
+class IgnitionSystem:
+    def create_spark(self):
+        pass
+
+
+class ElectricalSystem:
+    def switch_on(self):
+        pass
+
+
+class Car:
+    def __init__(self):
+        self.intake_system = IntakeSystem()
+        self.fuel_system = FuelSystem()
+        self.ignition_system = IgnitionSystem()
+        self.electrical_system = ElectricalSystem()
+
+    def start_engine(self):
+        self.electrical_system.switch_on()
+        self.intake_system.open_throttle()
+        self.fuel_system.inject_fuel()
+        self.ignition_system.create_spark()
+
+
+if __name__ == "__main__":
+    car = Car()
+    car.start_engine()
---
+++ tests/design_patterns/structural/facade_pattern/test_facade_pattern.py
@@ -0,0 +1,22 @@
+from unittest.mock import patch
+
+from design_patterns.structural.facade_pattern.facade_pattern import *
+
+
+class TestCar:
+    @patch.object(ElectricalSystem, 'switch_on')
+    @patch.object(IntakeSystem, 'open_throttle')
+    @patch.object(FuelSystem, 'inject_fuel')
+    @patch.object(IgnitionSystem, 'create_spark')
+    def test_start_engine(self, mocked_create_spark, mocked_inject_fuel, mocked_open_throttle, mocked_switch_on):
+        # Assign
+        car = Car()
+
+        # Act
+        car.start_engine()
+
+        # Assert
+        car.electrical_system.switch_on.assert_called_once()
+        car.intake_system.open_throttle.assert_called_once()
+        car.fuel_system.inject_fuel.assert_called_once()
+        car.ignition_system.create_spark.assert_called_once()
